f4039f5dd907c51fefed60e3f7f85e32ecbd3b50
Irteza Syed <irteza.m.syed@gmail.com>
2017-06-20 13:07:17 -0400

    Removed submit from register.php

--- api/register.php
+++ api/register.php
@@ -2,51 +2,49 @@
   session_start();
   include_once("config.php");
 
-  if(isset($_POST['submit'])){
-    if(isset($_POST['email']) && isset($_POST['password'])){
+  if(isset($_POST['email']) && isset($_POST['password'])){
 
-      $email = $_POST['email'];
-      $password=$_POST['password'];
-      $sql=$dbh->prepare("SELECT COUNT(*) FROM `users` WHERE `email`=?");
-      $sql->execute(array($_POST['email']));
+    $email = $_POST['email'];
+    $password=$_POST['password'];
+    $sql=$dbh->prepare("SELECT COUNT(*) FROM `users` WHERE `email`=?");
+    $sql->execute(array($_POST['email']));
 
-      if($sql->fetchColumn()!=0){
+    if($sql->fetchColumn()!=0){
+      $response["error"] = 1;
+      $response["success"] = 0;
+      $response["email"] = $email;
+      $response["result"] = 0;
+    }
+    else
+    {
+      function rand_string($length) {
+        $str="";
+        $chars = "subinsblogabcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $size = strlen($chars);
+        for($i = 0;$i < $length;$i++) {
+          $str .= $chars[rand(0,$size-1)];
+        }
+        return $str;
+      }
+
+      $p_salt = rand_string(20);
+      $site_salt="subinsblogsalt"; /*Common Salt used for password storing on site.*/
+      $salted_hash = hash('sha256', $password.$site_salt.$p_salt);
+
+      $sql=$dbh->prepare("INSERT INTO `users` (`ID`, `email`, `password`, `psalt`) VALUES (NULL, ?, ?, ?);");
+      if($sql->execute(array($_POST['email'], $salted_hash, $p_salt)) == TRUE) {
+        $response["error"] = 0;
+        $response["success"] = 1;
+        $response["email"] = $email;
+        $response["result"] = 1;
+      }
+      else{
         $response["error"] = 1;
         $response["success"] = 0;
         $response["email"] = $email;
-        $response["result"] = 0;
+        $response["result"] = 2;
       }
-      else
-      {
-        function rand_string($length) {
-          $str="";
-          $chars = "subinsblogabcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-          $size = strlen($chars);
-          for($i = 0;$i < $length;$i++) {
-            $str .= $chars[rand(0,$size-1)];
-          }
-          return $str;
-        }
-
-        $p_salt = rand_string(20);
-        $site_salt="subinsblogsalt"; /*Common Salt used for password storing on site.*/
-        $salted_hash = hash('sha256', $password.$site_salt.$p_salt);
-
-        $sql=$dbh->prepare("INSERT INTO `users` (`ID`, `email`, `password`, `psalt`) VALUES (NULL, ?, ?, ?);");
-        if($sql->execute(array($_POST['email'], $salted_hash, $p_salt)) == TRUE) {
-          $response["error"] = 0;
-          $response["success"] = 1;
-          $response["email"] = $email;
-          $response["result"] = 1;
-        }
-        else{
-          $response["error"] = 1;
-          $response["success"] = 0;
-          $response["email"] = $email;
-          $response["result"] = 2;
-        }
-      }
-      echo json_encode($response);
     }
+    echo json_encode($response);
   }
 ?>
